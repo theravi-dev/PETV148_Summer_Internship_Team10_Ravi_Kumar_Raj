732d3caa7728641d9d9e15f0aa73f12a4eceb1ad
Ravi Kumar Raj <ravirajmth021@gmail.com>
2026-07-06 11:45:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,5 +103,8 @@ def download():
             "attachment; filename=search_results.csv"
         }
     )
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
